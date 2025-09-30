84923381f3005c81e1a6ff3d6f27a407aa578852
Miguel Garcia <miguelgarciavalentin@gmail.com>
2025-09-30 11:18:38 -0400

    semana08-dia02-01-js-fetch-api-promise

---
+++ semana08/dia02-01-js-api-promises/main.js
@@ -0,0 +1,28 @@
+// Petición -> Request (Consultar información a un servidor. Ej. API (Application Programming Interface) y además podemos esperar a la respuesta (Response)
+
+// Ejemplo: consultar informacion de películas, listar productos, guardar un pedido, actualizar la informacion de una cita médica, eliminar una categoria.
+
+// Para consultar esa información lo hacemos usando fetch API.
+
+// Tipos de peticiones
+// GET -> Obtener datos (El método por defecto la fetch API)
+// POST -> Enguar datos para que sean guardados
+// PUT / PATH -> Actualizar datos que ya existen
+// DELETE -> Eliminar datos
+
+// Extensión de chrome para formatear JSON 
+// https://chromewebstore.google.com/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa?hl=en&pli=1
+
+
+// OBJETIVO: Consultar información en formato JSON desde una URL desde JS
+
+console.log(fetch('https://jsonplaceholder.typicode.com/posts')) // Devuelve una promesa (Promise) -> Promise {<pending>}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json()) // Convertimos la respuesta a un objeto JS
+  .then(data => {
+    console.log(data) // Aquí podemos usar el objeto JS
+
+       console.log(data[0].title)
+  })
+
